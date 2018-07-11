b65c647a3728921f563eaf7b388f40fce777b3bb
Drew Lichtenstein <drew_lichtenstein@harvard.edu>
2018-07-11 18:39:46 +0000

    Needs one more look over, but almost ready to submit.

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # Project 1
 
-I had a lot of problems with this!
+Overall, working on this project was really rewarding; I really struggled at first, but I eventually got things to work after many hours
+of practice -- programming can be quite rewarding!
+
+One requirement I couldn't quite figure out how to successfully implement the %LIKE% operator in SQL with variables -- I tried several
+different iteration but can't seem to get the syntax to work. I left my last attempt in as a comment (it's in the search_location
+function) and any suggestions on how to get it to work would be appreciated! As a result, my search result requires a specific location
+to work.
+
+Another thing I had some issue with was requiring users to be logged in; I have sessions set correctly (I think) so I know if someone is
+logged in and logging out works. What I am not quite clear on is how to track that someone is not logged in -- I tried to cover it
+with a try/except clause (see the search_location function), but no success and seeing if the session['user'] varialbe is set to false
+also didn't work. Thoughts on how I should implement this functionality?
+
+I like my solution for only letting them comment one: I render different HTML pages (one with the check-in form and one without) if their
+Username and Location ID match in the comments database!
+
+I did a bit of CSS styling (imported Bootstrap and customized error messages), but since I was a bit time crunched working on this project I
+didn't make it look too "pretty" -- definitely could improve more on that.
+
+Also, small thing, but I called my API address ("/api/zipcode/<lookup_zipcode>") instead of the required ("/api/zipcode/<lzip>")
+because "zip" seems to have a specific meaning in Python and I didn't want to accidentally cause any errors.
+
+All comments deeply appreciated -- I am taking this course to learn! :)
+
+
+
+
--- application.py
+++ application.py
@@ -2,6 +2,7 @@ import os
 import requests, json
 
 from flask import Flask, session, render_template, request, jsonify
+from passlib.hash import pbkdf2_sha256
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -22,53 +23,64 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-#this is OK!
+#default page is login
 @app.route("/")
 def index():
     return render_template('login.html')
 
-
-
+#registration page -- the /on_registration function below handles registration
 @app.route("/register")
 def registration ():
     return render_template('registration.html')
 
+#login page -- the /logged_in function below handles logging in
 @app.route("/login")
 def login():
     return render_template('login.html')
 
-@app.route("/on_submit", methods=["POST"])
-def on_submit ():
+# registration function: register username and hashed password after making sure the username is not taken
+@app.route("/on_registration", methods=["POST"])
+def on_registration ():
     username = request.form.get("username")
-    password = request.form.get("password")
+    password_no_hash = request.form.get("password")
+    password = pbkdf2_sha256.hash(password_no_hash)
+    if db.execute("SELECT * FROM user_login WHERE username = :username", {"username":username}).rowcount == 0:
+        db.execute("INSERT INTO user_login (username, password) VALUES (:username, :password)", {"username": username, "password": password})
+        db.commit()
+        return render_template('login.html')
+    else:
+        return render_template('error.html', message="Username taken! Please try registeroing again with a different username.")
 
-    db.execute("INSERT INTO user_login (username, password) VALUES (:username, :password)",
-            {"username": username, "password": password})
-    db.commit()
-    #db.execute("INSERT INTO check_in(username) VALUES(:username)", {"username":username})
-    #db.commit()
-    return render_template('login.html')
-
+# verify log-in is corect
 @app.route("/logged_in", methods=["POST"])
 def logged_in ():
     username = request.form.get("username")
     password = request.form.get("password")
-    if db.execute("SELECT * FROM user_login WHERE username = :username AND password = :password", {"username": username, "password": password}).rowcount == 0:
-        return render_template('error.html', message="Incorrect username and/or password. Try logging in again.")
-    else:
-        session['user'] = username
-        return render_template('main.html')
+    print(password)
+    if db.execute("SELECT * FROM user_login WHERE username = :username", {"username": username}).rowcount == 0:
+        return render_template('error.html', message="Incorrect username and/or password. Try logging in again or registering.")
+    password_to_check = db.execute("SELECT password FROM user_login WHERE username = :username",{"username":username}).fetchone()
+    verify_password = password_to_check.password
+    if pbkdf2_sha256.verify(password, verify_password) == False:
+        return render_template('error.html', message='Incorrect password.')
+    session['user'] = username
+    return render_template('main.html')
 
 @app.route("/search_location", methods=["POST"])
 def search_location():
-    location_search = request.form.get("location")
-    # Change the search for location to upper case since that's how it is in the database
-    location_search = location_search.upper()
-    # I couldn't get the syntax for a LIKE search to work, though I tried a lot of different iterations on %x% with the variable in the middle to no success.
-    # Any feedback on how to do one (I left my final example below) would be appreciated!
-   # location_result = db.execute("SELECT id, zipcode, city, state FROM zip_data WHERE zipcode LIKE '%'+:location_search+'%' OR city LIKE '%'+:location_search+'%' OR state LIKE '%'+:location_search+'%'", {"location_search":location_search}).fetchall()
-    location_result = db.execute("SELECT id, zipcode, city, state FROM zip_data WHERE city=:location_search",{"location_search":location_search}).fetchall()
-    return render_template('search_location.html',location_result=location_result)
+    try:
+        location_search = request.form.get("location")
+        # Change the search for location to upper case since that's how it is in the database
+        location_search = location_search.upper()
+        # I couldn't get the syntax for a LIKE search to work, though I tried a lot of different iterations on %x% with the variable in the middle to no success.
+        # Any feedback on how to do one (I left my final example below) would be appreciated!
+        # location_result = db.execute("SELECT id, zipcode, city, state FROM zip_data WHERE zipcode LIKE '%'+:location_search+'%' OR city LIKE '%'+:location_search+'%' OR state LIKE '%'+:location_search+'%'", {"location_search":location_search}).fetchall()
+        location_result = db.execute("SELECT id, zipcode, city, state FROM zip_data WHERE city=:location_search",{"location_search":location_search}).fetchall()
+        return render_template('search_location.html',location_result=location_result)
+
+# I tried to throw a Key Error when they aren't logged in, but it did not work the way I thought it would.
+    except KeyError:
+        return render_template('error.html', message="Please log-in.")
 
 @app.route("/search_location/<int:location_id>")
 def location(location_id):
@@ -79,15 +91,19 @@ def location(location_id):
     if location is None:
         return render_template("error.html", message="No such location exists")
 
+    # Fetch comments from our database and weather from Dark Sky
     comments = db.execute("SELECT comment FROM location_comments WHERE ref_id = :id", {"id":location_id}).fetchall()
     weather = requests.get(f"https://api.darksky.net/forecast/55d231cd30289abbca6266f1bdb90dc0/{location_info.latitude},{location_info.longitude}")
     weather_json = weather.json()
 
+    # If the user has not commented on this site before, render a template with the check-in form. If they have checked-in before, render a template without
+    # the form.
     if db.execute("SELECT * FROM location_comments WHERE username=:username AND ref_id=:location_id", {"username":session['user'], "location_id":location_id}).rowcount == 0:
         return render_template("location_info.html",location_info=location_info,comments=comments,weather_json=weather_json)
     else:
         return render_template("location_info_noform.html", location_info=location_info,comments=comments,weather_json=weather_json)
 
+# app for checking in inserts comments to the location comments table and increments the check-in counter in the zip_data table
 @app.route("/check_in", methods=["POST"])
 def check_in():
     comment = request.form.get("comment")
@@ -99,9 +115,9 @@ def check_in():
     return render_template("thanks.html")
 
 @app.route("/api/zipcode/<lookup_zipcode>")
-# I called it "lookup_zipcode" instead of "zip" because "zip" seems to have a specific meaning in Python
+# I called it "lookup_zipcode" instead of "zip" because "zip" seems to have a specific meaning in Python and I didn't want to accidentally cause any errors.
 def location_api(lookup_zipcode):
-   #Return details about a single location.
+   # Return details about a single location.
 
     # Make sure location exists.
     zip_result = db.execute("SELECT * FROM zip_data WHERE zipcode=:lookup_zipcode",{"lookup_zipcode":lookup_zipcode}).fetchone()
@@ -117,12 +133,10 @@ def location_api(lookup_zipcode):
             "check_ins": zip_result.check_in
         })
 
-
-
-
 @app.route("/logout")
 def logout ():
     session.pop('user', None)
+    session.get('logged_in') == False
     return render_template("login.html")
 
 @app.route("/main")
